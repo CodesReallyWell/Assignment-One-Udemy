03ef867abe936abc5c09bbe8daab081db4f928f2
CodesReallyWell <liamanewton@gmail.com>
2020-01-23 11:17:35 -0600

    Updated

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import './App.css'
+import UserInput from './Components/UserInput/UserInput'
+import UserOutput from './Components/UserOutput/UserOutput'
 
-function App() {
+const App = props => {
+
+  const [usernameState, setUserNameState] = useState({
+    users:[
+      { user: 'Liam', pass: 'memes'}
+    ]
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <UserInput/>
+      <UserOutput userName={usernameState.users[0].user}/>
     </div>
-  );
+  )
 }
 
 export default App;
